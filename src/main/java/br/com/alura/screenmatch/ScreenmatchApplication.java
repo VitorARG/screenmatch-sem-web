6f0df7d86008e6f3a23b51af69fe20ece653cd8e
VitorARG <vitor.augusto.6992@gmail.com>
2026-02-11 01:47:42 -0300

    aprimorando o uso de stream

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,12 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoApi consumirApi = new ConsumoApi();
-		var jason = consumirApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6b9ac889");
-		System.out.println(jason);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(jason, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibMenu();
 
 
 
